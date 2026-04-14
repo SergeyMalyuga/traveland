4722398415bc020c76332f5a38f82cd1e767573d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-14 16:13:13 +0300

    feat(constants): создать константу данных карточек локаций

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,7 +1,42 @@
+import type {Location} from "../models/Location.ts";
+
 export const Hooks = {
     HEADER: '[data-hook="header"]',
     HERO: '[data-hook="hero"]',
-    GALLERY: '[data-hook="gallery"]',
+    POPULAR: '[data-hook="popular"]',
     NEW_COLLECTION: '[data-hook="new-collection"]',
     FOOTER: '[data-hook="footer"]',
 } as const;
+
+export const LOCATIONS: Location[] = [
+    {
+        image: {
+            url: '/images/raster/istanbul.jpg',
+            width: 360,
+            height: 331,
+            alt: 'Istanbul, Turkey'
+        },
+        title: 'Istanbul, Turkey',
+        description: 'Istanbul is a major city in Turkey that straddles Europe.'
+    },
+    {
+        image: {
+            url: '/images/raster/kuala.jpg',
+            width: 360,
+            height: 331,
+            alt: 'Kuala Lumpur, malaysia'
+        },
+        title: 'Kuala Lumpur, malaysia',
+        description: 'A 272-step long trek leads you to this century.'
+    },
+    {
+        image: {
+            url: '/images/raster/seoul.jpg',
+            width: 360,
+            height: 331,
+            alt: 'Seoul, South Korea'
+        },
+        title: 'Seoul, South Korea',
+        description: 'South Korea officially the Republic of Korea is a country in East Asia.'
+    }
+]
