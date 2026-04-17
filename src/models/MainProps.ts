00cb823410a0ef42bab542219673c2d54ff33b72
Sergey Malyuga <sergey@malyuga.ru>
2026-04-17 11:53:49 +0300

    feat(models): добавить свойство секции офферов в модель опций главной страницы

--- src/models/MainProps.ts
+++ src/models/MainProps.ts
@@ -1,9 +1,11 @@
 import type {Header} from "../components/header/Header.ts";
 import type {Hero} from "../components/hero/Hero.ts";
 import type {Popular} from "../components/popular/Popular.ts";
+import type {Offers} from "../components/offers/Offers.ts";
 
 export interface MainProps {
     header: Header;
     hero: Hero;
     popular: Popular;
+    offers: Offers;
 }
