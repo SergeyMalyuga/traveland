4868f92cd6efd00094c42de26e0f8e477e3aa6b5
Sergey Malyuga <sergey@malyuga.ru>
2026-04-17 14:25:34 +0300

    feat(offer-card): создать компонент карточки оффера

---
+++ src/components/offer-card/OfferCard.ts
@@ -0,0 +1,28 @@
+import styles from './OfferCard.module.scss';
+import type {Offer} from "../../models/Offer.ts";
+
+export class OfferCard {
+    private offer: Offer;
+
+    constructor(offer: Offer) {
+        this.offer = offer;
+    }
+
+    public render() {
+        const {url, width, height, alt} = this.offer.image;
+        return `
+        <article class="${styles.card}">
+        <img class="${styles.image}" src="${url}" width="${width}" height="${height}" alt="${alt}" loading="lazy"/>
+        <div class="${styles.wrapperInfo}">
+          <h3 class="${styles.title}">${this.offer.title}</h3>
+          <div class="${styles.wrapperCountry}">
+            <span class="${styles.country}">${this.offer.country}</span>
+            <span class="${styles.rating}">${this.offer.rating}</span>
+          </div>
+          <span>${this.offer.days} Days ${this.offer.nights} nights</span>
+          <data value="${this.offer.price}"><span>$${this.offer.price}</span>/person</data>
+</div>
+</article>
+        `
+    }
+}
