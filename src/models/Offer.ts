7397959ad62a302ec73297f0beb28a64ab44d41d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-17 14:27:48 +0300

    feat(models): создать модель карточки оффера

---
+++ src/models/Offer.ts
@@ -0,0 +1,14 @@
+export interface Offer {
+    image: {
+        url: string;
+        width: number;
+        height: number;
+        alt: string;
+    },
+    title: string,
+    country: string;
+    rating: number;
+    days: number;
+    nights: number;
+    price: number;
+}
