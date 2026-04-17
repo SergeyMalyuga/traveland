fe2cef0e62bbc1f0cbe4d66a5f00fdbd663a2538
Sergey Malyuga <sergey@malyuga.ru>
2026-04-17 14:29:04 +0300

    feat(consts): добавить константные данные карточек офферов

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,4 +1,5 @@
 import type {Location} from "../models/Location.ts";
+import type {Offer} from "../models/Offer.ts";
 
 export const Hooks = {
     HEADER: '[data-hook="header"]',
@@ -37,5 +38,78 @@ export const LOCATIONS: Location[] = [
         },
         title: 'Seoul, South Korea',
         description: 'South Korea officially the Republic of Korea is a country in East Asia.'
+    }
+]
+
+export const OFFERS: Offer[] = [
+    {
+        image: {
+            url: '/images/raster/statue-of-liberty.jpg',
+            width: 262,
+            height: 173,
+            alt: 'Statue of Liberty'
+        },
+        title: 'Statue of Liberty',
+        country: 'New York',
+        rating: 4.8,
+        days: 4,
+        nights: 5,
+        price: 450
+    },
+    {
+        image: {
+            url: '/images/raster/great-wall .jpg',
+            width: 262,
+            height: 173,
+            alt: 'The Great Wall'
+        },
+        title: 'The Great Wall',
+        country: 'China',
+        rating: 4.9,
+        days: 7,
+        nights: 8,
+        price: 850
+    },
+    {
+        image: {
+            url: '/images/raster/dizin.jpg',
+            width: 262,
+            height: 173,
+            alt: 'Dizin'
+        },
+        title: 'Dizin',
+        country: 'Iran',
+        rating: 4.6,
+        days: 5,
+        nights: 6,
+        price: 550
+    },
+    {
+        image: {
+            url: '/images/raster/sajek-valley.jpg',
+            width: 262,
+            height: 173,
+            alt: 'Sajek Valley'
+        },
+        title: 'Sajek Valley',
+        country: 'Bangladesh',
+        rating: 4.7,
+        days: 3,
+        nights: 4,
+        price: 400
+    },
+    {
+        image: {
+            url: '/images/raster/dizin.jpg',
+            width: 262,
+            height: 173,
+            alt: 'Osaka Castle'
+        },
+        title: 'Osaka Castle',
+        country: 'Japan',
+        rating: 4.7,
+        days: 4,
+        nights: 5,
+        price: 700
     }
 ]
