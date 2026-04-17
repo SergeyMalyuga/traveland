a7df0c3208ba8c3343c8677da078a2d2b978b032
Sergey Malyuga <sergey@malyuga.ru>
2026-04-17 11:38:07 +0300

    feat(offers): интегрировать на главную страницу

--- src/pages/main/Main.ts
+++ src/pages/main/Main.ts
@@ -4,18 +4,21 @@ import type {Header} from "../../components/header/Header.ts";
 import type {Hero} from "../../components/hero/Hero.ts";
 import {Hooks} from "../../constants/consts.ts";
 import type {Popular} from "../../components/popular/Popular.ts";
+import type {Offers} from "../../components/offers/Offers.ts";
 
 export class Main {
     private container: HTMLElement;
     private header: Header;
     private hero: Hero;
     private popular: Popular;
+    private offers: Offers;
 
     constructor(container: HTMLElement, props: MainProps) {
         this.container = container;
         this.header = props.header;
         this.hero = props.hero;
         this.popular = props.popular;
+        this.offers = props.offers;
     }
 
     public render() {
@@ -24,10 +27,12 @@ ${this.header.render()}
 <main role="main">
         <div data-hook="hero"></div>
         <div data-hook="popular"></div>
+        <div data-hook="offers"></div>
 </main>
         `;
         this.mount(this.hero.render(), Hooks.HERO);
         this.mount(this.popular.render(), Hooks.POPULAR);
+        this.mount(this.offers.render(), Hooks.OFFERS);
     }
 
     private mount(element: HTMLElement, data: DataHook) {
