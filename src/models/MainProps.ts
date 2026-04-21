b2283fe5fca4a03f757ef9f26dee480aaea6d67c
Sergey Malyuga <sergey@malyuga.ru>
2026-04-21 12:18:05 +0300

    feat(main): добавить свойство новостей в модель главной страницы

--- src/models/MainProps.ts
+++ src/models/MainProps.ts
@@ -2,10 +2,12 @@ import type {Header} from "../components/header/Header.ts";
 import type {Hero} from "../components/hero/Hero.ts";
 import type {Popular} from "../components/popular/Popular.ts";
 import type {Offers} from "../components/offers/Offers.ts";
+import type {News} from "../components/news/News.ts";
 
 export interface MainProps {
     header: Header;
     hero: Hero;
     popular: Popular;
     offers: Offers;
+    news: News;
 }
