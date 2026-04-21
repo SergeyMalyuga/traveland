b7b50bb4f05c847ef2dd2cb7f7a77b0f75210e57
Sergey Malyuga <sergey@malyuga.ru>
2026-04-21 12:14:56 +0300

    feat(news): создать модель карточки новости

---
+++ src/models/News.ts
@@ -0,0 +1,11 @@
+export interface News {
+    image: {
+        url: string;
+        width: number;
+        height: number;
+        alt: string;
+    },
+    title: string;
+    description: string;
+    author: string;
+}
